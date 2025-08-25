1ff3a9d2fcce164726a9b6b3ab1d7289b999a2a8
serialthinker <zainabkurji@gmail.com>
2025-08-25 05:51:54 +0530

    Delete index.js

--- index.js
+++
@@ -1,1 +0,0 @@
-import React from "react"; import ReactDOM from "react-dom"; import CompoundPitchSite from "./CompoundPitchSite"; ReactDOM.render(<CompoundPitchSite />, document.getElementById("root"));
